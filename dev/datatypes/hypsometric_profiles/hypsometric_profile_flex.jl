1734722ebdfbd24962f165b9410d349c1ecb598f
daniel.lincke.globalclimateforum.org <daniel.lincke@clobalclimateforum.org>
2023-07-20 14:58:50 +0200

    recent changes

--- dev/datatypes/hypsometric_profiles/hypsometric_profile_flex.jl
+++ dev/datatypes/hypsometric_profiles/hypsometric_profile_flex.jl
@@ -1,28 +1,30 @@
 using Main.ExtendedLogging
 using Logging
 
-export exposure, sed, sed_above, sed_below, remove_below, add_above, add_between
+#export exposure, sed, sed_above, sed_below, remove_below, add_above, add_between
+
 
 mutable struct HypsometricProfileFlex
-  width        :: Float64
-  minElevation :: Float64
-  maxElevation :: Float64
+  width        :: Float32
+  minElevation :: Float32
+  maxElevation :: Float32
 
-  exposure     :: Dict{Float64,(Float64,Float64,Float64)}
+  exposure     :: Dict{Float32,(Float32,Float32,Float32)}
  
-  elevation  :: Array{Float64}
-  area       :: Array{Float64}
-  population :: Array{Float64}
-  assets     :: Array{Float64}
+  elevation  :: Array{Float32}
+  area       :: Array{Float32}
+  population :: Array{Float32}
+  assets     :: Array{Float32}
   logger     :: ExtendedLogging.ExtendedLogger
 
+#=
   function HypsometricProfileFlex(w,m,x1,x2,x3,x4,logger)
     if (length(x1)!=length(x2)) Main.ExtendedLogging.log(logger,Logging.Error,@__FILE__,"\n length(x1) != length(x2) as length($x1) != length($x2) as $(length(x1)) != $(length(x2))") end
     if (length(x1)!=length(x3)) Main.ExtendedLogging.log(logger,Logging.Error,@__FILE__,"\n length(x1) != length(x3) as length($x1) != length($x3) as $(length(x1)) != $(length(x3))") end
-    if (length(x1)!=length(x4)) Main.ExtendedLogging.log(logger,Logging.Error,@__FILE__,"\n length(x1) != length(x4) as length($x1) != length($x4) as $(length(x1)) != $(length(x4))") end
+    if (length(x1)!=length(x4)) Main.ExtendedLogging.log(logger,Logging.Error,@__FILE__,"\n length(x1) != length(x4) as length($x1) != length($x4) as $(length(x1)) != $(length(x4))") end#
 
     if (length(x1) < 1) Main.ExtendedLogging.log(logger,Logging.Error,@__FILE__,"\n length(x1) = length($x1) = $(length(x1)) < 2 which is not allowed") end
-    if (x1[1] < m) Main.ExtendedLogging.log(logger,Logging.Error,@__FILE__,"\n x1[1] = $(x1[1]) < minimum elevation m = $m which is not allowed") end
+   if (x1[1] < m) Main.ExtendedLogging.log(logger,Logging.Error,@__FILE__,"\n x1[1] = $(x1[1]) < minimum elevation m = $m which is not allowed") end#
 
     delta = x1[1] - m
     for x in 2:(length(x1)) 
@@ -32,10 +34,11 @@ mutable struct HypsometricProfileFlex
     if (!issorted(x1)) Main.ExtendedLogging.log(logger,Logging.Error,@__FILE__,"\n x1 is not sorted: $x1") end
     new(w,m,x1[length(x1)],delta,pushfirst!(x1,m),pushfirst!(x2,0),pushfirst!(x3,0),pushfirst!(x4,0), cumsum(x2), cumsum(x3), cumsum(x4), logger)
   end
-
+=#
 end
 
 
+#=
 function exposure(hspf :: HypsometricProfileFlex, e) 
 end
 
@@ -56,4 +59,4 @@ end
 
 function add_between(hspf, above, below, pop, assets)
 end
-
+=#
