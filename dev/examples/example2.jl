e2274ca8e6f19e8f055e8dd4d0b5b8c6eddca138
daniel.lincke.globalclimateforum.org <daniel.lincke@clobalclimateforum.org>
2023-07-20 14:58:36 +0200

    recent changes

--- dev/examples/example2.jl
+++ dev/examples/example2.jl
@@ -11,7 +11,11 @@ simple_logger = Logging.SimpleLogger(io)
 logger = Main.ExtendedLogging.ExtendedLogger(simple_logger,io,now())
 
 using Main.jdiva.HypsometricProfiles
-hp = Main.jdiva.HypsometricProfiles.HypsometricProfileFixed(1, 0, [1, 2, 3, 4, 5], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], logger)
+
+hp = Main.jdiva.HypsometricProfiles.HypsometricProfileClassical(1, 0, [1, 2, 3, 4, 5], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1], logger)
+
+println()
+println("orig:")
 
 println(hp)
 
@@ -19,54 +23,67 @@ println(Main.jdiva.HypsometricProfiles.exposure(hp,-1))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,2))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,7))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,4.9))
+
+
+println()
+println("sed(hp,0.9,1.2):")
 
 sed(hp,0.9,1.2)
-
 println(hp)
 
 println(Main.jdiva.HypsometricProfiles.exposure(hp,-1))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,2))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,7))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,4.9))
+
+println()
+println("sed_above(hp,2.5,0.9,1.2):")
 
 sed_above(hp,2.5,0.9,1.2)
-
 println(hp)
 
 println(Main.jdiva.HypsometricProfiles.exposure(hp,-1))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,2))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,7))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,4.9))
+
+println()
+println("sed_above(hp,2.5,0.9,1.2):")
 
 sed_below(hp,2.5,0.9,1.2)
-
 println(hp)
 
 println(Main.jdiva.HypsometricProfiles.exposure(hp,-1))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,2))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,7))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,4.9))
+
+println()
+println("remove_below(hp,3.8):")
 
 println(remove_below(hp,3.8))
-
 println(hp)
 
 println(Main.jdiva.HypsometricProfiles.exposure(hp,-1))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,2))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,7))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,4.9))
+
+println()
+println("remove_below(hp,10):")
 
 println(remove_below(hp,10))
-
 println(hp)
 
 println(Main.jdiva.HypsometricProfiles.exposure(hp,-1))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,2))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,7))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,4.9))
+
+println()
+println("add_above(hp,0,10,100)")
 
 add_above(hp,0,10,100)
-
 println(hp)
 
 println(Main.jdiva.HypsometricProfiles.exposure(hp,-1))
@@ -74,10 +91,10 @@ println(Main.jdiva.HypsometricProfiles.exposure(hp,2))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,7))
 println(Main.jdiva.HypsometricProfiles.exposure(hp,4.9))
 
-println(remove_below(hp,10))
+println()
+println("add_between(hp,2,3,10,100)")
 
 add_between(hp,2,3,10,100)
-
 println(hp)
 
 println(Main.jdiva.HypsometricProfiles.exposure(hp,-1))
