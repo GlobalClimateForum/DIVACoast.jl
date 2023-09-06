cd921c75e745c3a93cfe6f2c54df97fbef772652
daniel.lincke.globalclimateforum.org <daniel.lincke@clobalclimateforum.org>
2023-09-06 10:34:51 +0200

    Daniel: update

--- dev/datatypes/hypsometric_profiles/hypsometric_profiles.jl
+++ dev/datatypes/hypsometric_profiles/hypsometric_profiles.jl
@@ -1,9 +1,5 @@
-module HypsometricProfiles
-
 include("hypsometric_profile_fixed_array.jl")
 include("hypsometric_profile_fixed_classical.jl")
 include("hypsometric_profile_fixed_strarray.jl")
 include("hypsometric_profile_flex.jl")
-
-end
 
