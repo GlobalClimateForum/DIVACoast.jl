597a6e0b8be6d11f262bcc96a392a77cc6adbbd2
daniel.lincke.globalclimateforum.org <daniel.lincke@clobalclimateforum.org>
2023-10-25 13:40:33 +0200

    Daniel: fixed issues

--- dev/jdiva_lib.jl
+++ dev/jdiva_lib.jl
@@ -1,9 +1,11 @@
 module jdiva
 
+# constants
+const global earth_radius_km = 6371
+
 include("./logger/jdiva_logger.jl")
 include("./datatypes/geodatatype/SparseGeoArrays.jl")
 include("./datatypes/hypsometric_profiles/hypsometric_profiles.jl")
-
+include("./algorithms/conversion/SGRToHSP.jl")
 
 end
-
