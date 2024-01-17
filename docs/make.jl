3c4bf6b7859af2800f96a3ee0d0015eaf7363033
daniel.lincke.globalclimateforum.org <daniel.lincke@clobalclimateforum.org>
2024-01-17 12:25:30 +0100

    Daniel: doc updates

--- docs/make.jl
+++ docs/make.jl
@@ -6,8 +6,13 @@ using .jdiva
 using Documenter
 
 makedocs(
+    authors="Daniel Lincke et al <daniel.lincke@globalclimateforum.org>",
     sitename = "jdiva",
     modules  = [jdiva],
     remotes = nothing,
-    pages=["Home" => "index.md"]
+    pages=["Home" => "index.md"],
+    format = Documenter.HTML(
+        prettyurls = false,
+#        repolink="https://globalclimateforum.gitlab.io/diva_library/",)
+        repolink="https://gitlab.com/globalclimateforum/diva_library",)
     )
