0611ae69a638a04131fbd18b8e60650e21779a9b
daniel.lincke.globalclimateforum.org <daniel.lincke@clobalclimateforum.org>
2024-02-16 17:55:52 +0100

    DANIEL: inegration pimping

--- src/datatypes/coastal_model/local_coastal_model.jl
+++ src/datatypes/coastal_model/local_coastal_model.jl
@@ -9,16 +9,16 @@ mutable struct LocalCoastalModel{DT<:Real}
 end
 
 function expected_damage_standard_ddf(lcm::LocalCoastalModel{DT}, hdd_area::DT, hdds_static::Array{DT}, hdds_dynamic::Array{DT}) where {DT<:Real}
-  edam_area = quadgk(x -> (damage_standard_ddf(lcm.coastal_plain_model, :area, x, hdd_area) * pdf(lcm.surge_model, x)), 0, maximum(lcm.surge_model))[1]
+  edam_area = quadgk(x -> (damage_standard_ddf(lcm.coastal_plain_model, :area, x, hdd_area) * pdf(lcm.surge_model, x)), 0, maximum(lcm.surge_model), rtol=1e-2)[1]
   edam_static = Array{DT}(undef, size(lcm.coastal_plain_model.cummulativeStaticExposure)[2])
   edam_dynamic = Array{DT}(undef, size(lcm.coastal_plain_model.cummulativeDynamicExposure)[2])
 
   for ind in 1:size(lcm.coastal_plain_model.cummulativeStaticExposure, 2)
-    edam_static[ind] = quadgk(x -> (damage_standard_ddf(lcm.coastal_plain_model, lcm.coastal_plain_model.staticExposureSymbols[ind], x, hdds_static[ind]) * pdf(lcm.surge_model, x)), minimum(0), maximum(lcm.surge_model))[1]
+    edam_static[ind] = quadgk(x -> (damage_standard_ddf(lcm.coastal_plain_model, lcm.coastal_plain_model.staticExposureSymbols[ind], x, hdds_static[ind]) * pdf(lcm.surge_model, x)), minimum(0), maximum(lcm.surge_model), rtol=1e-2)[1]
   end
 
   for ind in 1:size(lcm.coastal_plain_model.cummulativeDynamicExposure, 2)
-    edam_dynamic[ind] = quadgk(x -> (damage_standard_ddf(lcm.coastal_plain_model, lcm.coastal_plain_model.dynamicExposureSymbols[ind], x, hdds_dynamic[ind]) * pdf(lcm.surge_model, x)), minimum(0), maximum(lcm.surge_model))[1]
+    edam_dynamic[ind] = quadgk(x -> (damage_standard_ddf(lcm.coastal_plain_model, lcm.coastal_plain_model.dynamicExposureSymbols[ind], x, hdds_dynamic[ind]) * pdf(lcm.surge_model, x)), minimum(0), maximum(lcm.surge_model), rtol=1e-2)[1]
   end
 
   (edam_area, edam_static, edam_dynamic)
@@ -31,9 +31,8 @@ expected_damage_standard_ddf(lcm::LocalCoastalModel{DT}, hdd_area::Real, hdds_st
     expected_damage_standard_ddf(lcm, convert(DT, hdd_area), convert(Array{DT}, hdds_static), hdds_dynamic)
   end
 
-
 function expected_damage_standard_ddf(lcm::LocalCoastalModel, s::Symbol, hdd::Real)
-  quadgk(x -> (damage_standard_ddf(lcm.coastal_plain_model, s, x, hdd) * pdf(lcm.surge_model, x)), minimum(0), maximum(lcm.surge_model))[1]
+  quadgk(x -> (damage_standard_ddf(lcm.coastal_plain_model, s, x, hdd) * pdf(lcm.surge_model, x)), minimum(0), quantile(lcm.surge_model,0.9999), rtol=1e-3)[1]
 end
 
 exposure_below(lcm::LocalCoastalModel{DT}, e::Real) where {DT<:Real} = exposure_below(lcm.coastal_plain_model, e)
