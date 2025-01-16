71d08f308dd73d8244de81476416b0d377fdc2c7
larsenno <lars.enhonsel@googlemail.com>
2025-01-16 17:15:57 +0100

    Added diva_data() function to jdiva_env and added jdiva header.

--- src/jdiva_env.jl
+++ src/jdiva_env.jl
@@ -1,6 +1,25 @@
+DRAW_HEADER = true
+
 # Activates the DIVA project environment (dependencies)
 # Include this script in your Julia code to activate the DIVA environment
 # Must be included before including the jdiva module !
 using Pkg
 Pkg.activate("$(ENV["DIVA_LIB"])")
 Pkg.instantiate()  
+
+export data
+
+if DRAW_HEADER
+    println("┌                             ┐")
+    println("│~~~~▗▖▗▄▄▄~~▗▄▄▄▖▗▖~~▗▖~▗▄▖~~│")
+    println("│~~~~▗▖▐▌~~█~~~█~~▐▌~~▐▌▐▌~▐▌~│")
+    println("│~▄~~▐▌▐▌~~█~~~█~~▐▌~~▐▌▐▛▀▜▌~│")
+    println("│~▀▄▄▞▘▐▙▄▄▀~▗▄█▄▖~▝▚▞▘~▐▌~▐▌~│")
+    println("│~...© GLOBAL CLIMATE FORUM .~│")
+    println("└                             ┘")
+end
+
+"""
+Returns the full path to the data directory.
+"""
+diva_data = (subdir) -> "$(ENV["DIVA_DATA"])/$(subdir)"
