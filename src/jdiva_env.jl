67bd52ed40bbae48eaa3ae35adb5118595cc702c
larsenno <lars.enhonsel@googlemail.com>
2025-01-16 11:51:12 +0100

    Some comments on the diva project environment

--- src/jdiva_env.jl
+++ src/jdiva_env.jl
@@ -1,3 +1,6 @@
+# Activates the DIVA project environment (dependencies)
+# Include this script in your Julia code to activate the DIVA environment
+# Must be included before including the jdiva module !
 using Pkg
 Pkg.activate("../.")
 Pkg.instantiate()  
