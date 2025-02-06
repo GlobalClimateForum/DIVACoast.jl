ec4fd4b8b6b784fbd644ac7ea04cd004d7751c11
larsenno <lars.enhonsel@googlemail.com>
2025-02-06 12:45:08 +0100

    Some comments on ssp wrapper

--- src/scenario/ssp_wrapper.jl
+++ src/scenario/ssp_wrapper.jl
@@ -22,6 +22,7 @@ ssp_get_growth(sw::SSPWrapper{GrowthFactor}, g::Real) = g - 1
 
 function ssp_get_growth_factor(wrapped_ssp::SSPWrapper{T}, variable::String, country::String, ssp_scenario::String, year1::Int, year2::Int, do_warn = true) where {T<:SSPType}
 
+    # Filter function to retrieve specifc ssp_scenario by variable and country
     function flt(v, r, s)
         v_filter = !ismissing(v) && v == variable
         r_filter = !ismissing(r) && r == country
@@ -29,17 +30,22 @@ function ssp_get_growth_factor(wrapped_ssp::SSPWrapper{T}, variable::String, cou
         return (v_filter && r_filter && s_filter)
     end
 
+    # switch years if year2 is before year1
     if (year2 < year1)
         year2, year1 = year1, year2
     end
 
+    # Calculate the number of years between both years
     ny = year2 - year1
     if (ny == 0)
-        return 1.0
+        return 1.0 # both years are the same
     end
 
+    # Get all available years from the ssp dataframe
     years_available = sort(unique(wrapped_ssp.df_ssp.year))
-    ind_y1 = searchsortedfirst(years_available, year1)
+    
+    # if year is not available index is length + 1
+    ind_y1 = searchsortedfirst(years_available, year1) 
     ind_y2 = searchsortedfirst(years_available, year2)
 
     filtered_df = filter([:Variable, :Region, :Scenario] => flt, wrapped_ssp.df_ssp)
@@ -49,6 +55,8 @@ function ssp_get_growth_factor(wrapped_ssp::SSPWrapper{T}, variable::String, cou
     end
 
     ret = 1.0
+
+    # 
     if (year1 <= years_available[1])
         if (year2 <= years_available[2])
             return ret * (1 + ssp_get_growth(wrapped_ssp, filtered_df[1, :].growth))^(ny)
@@ -63,10 +71,15 @@ function ssp_get_growth_factor(wrapped_ssp::SSPWrapper{T}, variable::String, cou
         if (ind_y1 > 1)
             ret = ret * (1 + ssp_get_growth(wrapped_ssp, filtered_df[ind_y1-1, :].growth))^(years_available[ind_y1] - year1)
         end
-        for ind in (ind_y1+1):ind_y2
-            if ind <= size(years_available, 1)
-                y2 = years_available[ind]
-                if y2 <= year2
+
+        # Calculate cummulative growth between year_1 and year_2
+        for ind in (ind_y1+1):ind_y2 # Iterate between years
+            
+            if ind <= size(years_available, 1) # Check if still in available range
+                
+                y2 = years_available[ind] # If in available range current year (years_available[current_i])
+                
+                if y2 <= year2 # If year is  not last year 
                     ret = ret * (1 + ssp_get_growth(wrapped_ssp, filtered_df[ind-1, :].growth))^(years_available[ind] - years_available[ind-1])
                 else
                     return ret * (1 + ssp_get_growth(wrapped_ssp, filtered_df[ind-1, :].growth))^(year2 - years_available[ind-1])
