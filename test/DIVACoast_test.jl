40eca7d7b9b854e65db357bab761fce89e6f5dc2
vanessa.voelz <vanessa.voelz@globalclimateforum.org>
2025-03-28 10:38:50 +0100

    activate divacoast with different toml files in test folder

---
+++ test/DIVACoast_test.jl
@@ -0,0 +1,66 @@
+using Pkg
+# Activates the DIVACoast.jl project environment (dependencies)
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+
+import YAML
+export earth_circumference_km, earth_radius_km
+
+# Read local library configuration
+global config = YAML.load_file(joinpath(@__DIR__, "DIVACoast.jl.yml"), dicttype = Dict{Symbol, Any})
+
+module DIVACoast
+export earth_circumference_km, earth_radius_km
+
+    # append depot path (local packages) to project load path
+    # append!(LOAD_PATH, DEPOT_PATH)
+
+    # Set constants from local config
+    earth_circumference_km = Main.config[:earthCircumferenceKM]
+    earth_radius_km = Main.config[:earthRadiusKM]
+
+    function __init__()
+
+        DRAW_HEADER = Main.config[:drawHeader]
+        SHORT_HEADER = Main.config[:shortHeader]
+    
+        # Header
+        if DRAW_HEADER && SHORT_HEADER
+            println("┌                                       ┐")
+            println("│ DIVACoast.jl | © GLOBAL CLIMATE FORUM │")
+            println("└                                       ┘")
+        elseif DRAW_HEADER
+            println("┌                                                      ┐")
+            println("│~▗▄▄▄~~▗▄▄▄▖▗▖~~▗▖~▗▄▖~~▗▄▄▖~▗▄▖~~▗▄▖~~▗▄▄▖▗▄▄▄▖▄~▗▖█~│")
+            println("│~▐▌~~█~~~█~~▐▌~~▐▌▐▌~▐▌▐▌~~~▐▌~▐▌▐▌~▐▌▐▌~~~~~█~~~~▗▖█~│")
+            println("│~▐▌~~█~~~█~~▐▌~~▐▌▐▛▀▜▌▐▌~~~▐▌~▐▌▐▛▀▜▌~▝▀▚▖~~█~▄~~▐▌█~│")
+            println("│~▐▙▄▄▀~▗▄█▄▖~▝▚▞▘~▐▌~▐▌▝▚▄▄▖▝▚▄▞▘▐▌~▐▌▗▄▄▞▘~~█~▀▄▄▞▘█~│")
+            println("│~~~~~~~~~~~~~~~~[©GLOBAL CLIMATE FORUM]~~~~~~~~~~~~~~~│")
+            println("└                                                      ┘")
+        end
+
+        # Include functions
+        include("./logger/logger.jl")
+        include("./datatypes/geodatatype/SparseGeoArrays.jl")
+        include("./datatypes/hypsometric_profiles/hypsometric_profiles.jl")
+        include("./datatypes/coastal_model/local_coastal_model.jl")
+        include("./datatypes/coastal_model/composed_coastal_model.jl")
+        include("./datatypes/coastal_model/composed_coastal_model_generics.jl")
+        include("./datatypes/geodatatype/nn.jl")
+        include("./algorithms/conversion/sgr_to_hsp.jl")
+        include("./algorithms/coastal/coastline.jl")
+        include("./algorithms/coastal/coastplain.jl")
+        include("./algorithms/statistics/gev_fits.jl")
+        include("./algorithms/statistics/gpd_fits.jl")
+        include("./algorithms/statistics/extreme_distributions_plot.jl")
+        include("./algorithms/numerical/simple_integration.jl")
+        include("./io/nc/HSPs_nc_load.jl")
+        include("./io/nc/HSPs_nc_save.jl")
+        include("./io/csv/ccm_indicator_datafame.jl")
+        include("./io/jld/jld_load.jl")
+        include("./tools/geotiff_tools.jl")
+        include("./scenario/ssp_scenario_reader.jl")
+        include("./scenario/slr_scenario_reader.jl")
+    end
+end
+
