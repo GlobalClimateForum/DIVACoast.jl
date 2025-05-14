1fe106e888433c95d4a11ea7fdec428b4f6805b6
larsenno <lars.enhonsel@gmail.com>
2025-05-14 18:57:36 +0200

    Documentation for Neighbour (nearest neighbour search)

--- src/datatypes/geodatatype/nn.jl
+++ src/datatypes/geodatatype/nn.jl
@@ -6,7 +6,7 @@ using Distances
 export Neighbour, nearest, nearest_coord, coords_to_wide
 
 """
-This funciton transforms the longitude and latitude columns of an DataFrame to a Matrix in wide-format required
+This function transforms the longitude and latitude columns of an DataFrame to a Matrix in wide-format required
 for Nearest Neighbour matching. 
 # Parameter
 - df: The input DataFrame containing the longitude and latitude column.
@@ -50,7 +50,26 @@ end
 
 
 """
-The Neighbours structure holds and BallTree Object and the created Matrix.
+    function Neighbour(df::DataFrame, dtype::Type; lonlatCols::Tuple{Union{String, Symbol}, Union{String, Symbol}} = (:lon, :lat), dropna::Bool = true)
+
+The Neighbour structure is a efficient way to access a `DataFrame` by coordinates. It therfore transforms the longitude and latitude columns `lonlatCols` of the input DataFrame to a Matrix
+in wide-format and creates a BallTree object. The BallTree object is then used to search for nearest neighbours efficiently.
+
+# Arguments
+- df: The input DataFrame containing the longitude and latitude column.
+- dtype: The datatype the coordinates will be parsed in.
+- lonlatCols (optional): The longitude and latitude columns (default = (:lon, :lat))
+- dropna (optional): Whether na's should be kept or not (default = true).
+
+# Example
+```julia
+using DataFrames
+using CSV
+
+observations = CSV.File("observations.csv") |> DataFrame # read observations.csv as DataFrame
+nObservations = Neighbour(observations, Float64, lonlatCols = (:longitude, :latitude), dropna = true) # create Neighbour object
+nearest(nObservations, (37.1100, -12.2877)) # search for nearest neighbour of coordinate (37.1100, -12.2877)) # search for nearest neighbour of coordinate (37.1100, -12.2877) in DataFrame
+```
 """
 struct Neighbour
   tree::BallTree
@@ -66,10 +85,17 @@ struct Neighbour
 end
 
 """
-The nearest function returns the index of nearest neighbour of an Neighbours Object to an coordinate.
-# Parameter
-  - n: The Neighbours Object to search trough.
-  - coordinate: A coordinate the nearest neighbour relates to.
+    function nearest(n::Neighbour, coordinate::Tuple)
+
+The nearest function performs a nearest neighbour search on the `Neighbours` Object
+
+# Arguments
+- n: The Neighbours Object to search trough.
+- coordinate: A coordinate the nearest neighbour relates to.
+
+# Return
+Returns a tuple (index, distance, info) containing the of the nearest neighbour in the `Neighbour.tree` object,
+the distance to the nearest neighbour and the information of the nearest neighbour in the `Neighbour.dataframe` object.
 """
 function nearest(n::Neighbour, coordinate::Tuple)
   lon, lat = coordinate
@@ -79,15 +105,30 @@ function nearest(n::Neighbour, coordinate::Tuple)
 end
 
 """
-The nearest function returns the nearest neighbour of an Neighbours Object to multiple coordinates.
-Coordinates can be passed to the fucntion as a DataFrame. 
-# Parameter
-  - n: The Neighbours Object to search trough.
-  - df: DataFrame holding the coordinates, the nearest neighbour should relate to.
-  - dtype: DataType the coordinates will be parsed in.
-  - lonlatCols: Columns names (string / symbol) of the columns in the input 
-  dataframe holding the coordinates. 
-  - dropna: Whether na's should be kept or not.
+Mathches two `DataFrames` by coordinates. The function uses the `nearest` function to find the nearest neighbour in a `Neighbour` object for each coordinate in the input DataFrame.
+
+# Arguments 
+- n: The Neighbours Object to search trough.
+- df: DataFrame holding the coordinates, the nearest neighbour should relate to.
+- dtype: DataType the coordinates will be parsed in.
+- lonlatCols: Columns names (string / symbol) of the columns in the input 
+  dataframe holding the coordinates.
+- dropna: Whether na's should be kept or not (default = true).
+
+# Return
+Returns a DataFrame containing the index, distance and information of the nearest neighbour for each coordinate in the input DataFrame.
+
+# Example
+```julia
+using DataFrames
+using CSV
+
+last_observations = CSV.File("last_observations.csv") |> DataFrame # read last_observations.csv as DataFrame
+new_observations = CSV.File("new_observations.csv") |> DataFrame # read new_observations.csv as DataFrame
+
+nlastObservations = Neighbour(last_observations, Float64, lonlatCols = (:longitude, :latitude), dropna = true)
+observations = nearest(nlastObservations, new_observations, Float64, lonlatCols = (:longitude, :latitude), dropna = true) 
+```
 """
 function nearest(n::Neighbour, df::DataFrame, dtype::Type ; 
   lonlatCols::Tuple{Union{String, Symbol}, Union{String, Symbol}} = (:lon, :lat), dropna::Bool = true)
@@ -96,7 +137,27 @@ function nearest(n::Neighbour, df::DataFrame, dtype::Type ;
 end
 
 """
-Does same as nearest() but returns Coordinate of nearest neighbour.
+    nearest_coord(n::Neighbour, coordinate::Tuple)
+    
+The nearest_coord function performs a nearest neighbour search on the `Neighbours` Object and returns the coordinates of the nearest neighbour.
+
+# Arguments
+- n: The Neighbours Object to search trough.
+- coordinate: A coordinate the nearest neighbour relates Tuple{Float64, Float64} (lon, lat) to.
+
+# Return
+Returns a tuple (lonN, latN) containing the coordinates of the nearest neighbour in the `Neighbour.tree` object.
+
+# Example
+```julia
+using DataFrames
+using CSV
+
+observations = CSV.File("observations.csv") |> DataFrame # read observations.csv as DataFrame
+nObservations = Neighbour(observations, Float64, lonlatCols = (:longitude, :latitude), dropna = true) # create Neighbour object
+
+nearest = nearest_coord(nObservations, (37.1100, -12.2877)) # search for nearest neighbour coordinate of coordinate (37.1100, -12.2877)
+```
 """
 function nearest_coord(n::Neighbour, coordinate::Tuple)
   lon, lat = coordinate
