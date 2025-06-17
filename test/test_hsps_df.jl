509322f18596241f02f64a67c9d8c7ce66135cc1
larsenno <lars.enhonsel@googlemail.com>
2025-06-17 15:18:37 +0200

    Minor Changes

--- test/test_hsps_df.jl
+++ test/test_hsps_df.jl
@@ -2,21 +2,29 @@ using Pkg
 Pkg.activate("$(ENV["DIVA_LIB"])")
 include("$(ENV["DIVA_LIB"])/src/DIVACoast.jl")
 using .DIVACoast
+using DataFrames
 
 
 hspfs = load_hsps_nc(Int32, Float32, "./testdata/UKIRL/nc/UKIRL_hspfs_floodplains.nc")
 
 
-h1 = deepcopy(hspfs[42]) 
-h2 = deepcopy(hspfs[42])
+h1 = deepcopy(hspfs[12]) 
+h2 = deepcopy(hspfs[12])
 
 
-h2 = to_DF(h2)
-h2.popShare = h2.population ./ maximum(h2.population)
-h2 = HypsometricProfile(h2, exposureCols = [:population, :assets, :popShare], exposureUnits = ["m", "m", "m"])
+h2 = DataFrame(h2)
+h2.popShare = (h2.population ./ maximum(h2.population)) * 100
+
+h2.assetsEUR = 0.85 * h2.assets
+
+
+h2 = HypsometricProfile(h2, exposureCols = [:population, :assets, :assetsEUR, :popShare], exposureUnits = ["", "USD", "EUR", "%"])
+
+
+
 
 # save(h2, "./hpfs_test.jld2")
 
-# display(h2)
-# print(h2)
-show(h2)
+
+print(h2)
+display(h2)
