b34e30ce70dc3e7239cda196af4ceb3d1331507d
daniel.lincke@globalclimateforum.org <daniel.lincke@globalclimateforum.org>
2025-09-09 13:25:11 +0200

    Added missing export

--- src/DIVACoast.jl
+++ src/DIVACoast.jl
@@ -33,7 +33,7 @@ export LinInt, linear_interp, support, probs, cdf, pdf, quantile, manual_integra
 export InundationModel, BathtubInundation, LinearDistanceAttenuatedInundation, inundate, water_depth, water_level, max_water_level
 
 # Export GeoArray adapter stuff
-export read_geotiff_to_geoarray_2d, empty_geo_array, is_circular, no_data_value, clear_data!
+export read_geotiff_to_geoarray_2d, empty_geo_array, empty_copy_from_geo_array, is_circular, no_data_value, clear_data!
 
 # Export GeoArray iterators
 export GeoArrayIndexValueIterator
