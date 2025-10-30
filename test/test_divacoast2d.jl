fb4be81b93afaf2cf0cc675459c3be1ed945044e
larsenno <lars.honsel@globalclimateforum.org>
2025-10-30 10:35:29 +0100

    Changed Tests for SpatialProfile

--- test/test_divacoast2d.jl
+++ test/test_divacoast2d.jl
@@ -6,10 +6,15 @@ using Plots
 using GeoArrays
 using CSV
 using DataFrames
+using Distributions
+using Statistics
+using Logging
 
 # helpers
 
-path_to_data = (subdir) -> joinpath(ENV["DIVA_LIB"], "testdata/uk_the_wash", subdir)
+global_logger(DIVALogger("./log.txt"))
+
+path_to_data = (subdir) -> joinpath(ENV["DIVA_DATA"], "spatial_test_data", subdir)
 
 function reclassify_wbm(wbm::GeoArrays.GeoArray)
 
@@ -32,22 +37,45 @@ na_mask_fabdem = (fabdem::GeoArrays.GeoArray, wbm::GeoArrays.GeoArray) -> begin
     return fabdem
 end
 
+
 # data pipeline
+@info "Loading test data"
 
-@info "data pipeline ..."
-
-wbm = "copdem_wbm.tif" |> path_to_data |> GeoArrays.read |> reclassify_wbm
+wbm = "wbm.tif" |> path_to_data |> GeoArrays.read |> reclassify_wbm
 fabdem = "fabdem.tif" |> path_to_data |> GeoArrays.read |> fabdem_ -> na_mask_fabdem(fabdem_, wbm)
 pop = "worldpop.tif" |> path_to_data |> GeoArrays.read
 corine = "corine.tif" |> path_to_data |> GeoArrays.read
-attrates = "corine_attrates.csv" |> path_to_data |> CSV.File |> DataFrame
+# attrates = "corine_attrates.csv" |> path_to_data |> CSV.File |> DataFrame
+
+@info "Initialize SpatialProfile"
+fp = SpatialProfile(fabdem; seed = findall(wbm .== true)[1], exposures = [pop])
+
+@info "Running inundation model tests"
+
+hcb = inundate(fp, 2.0, HydraulicConnectedBathtub())
+pba = inundate(fp, 2.0, PathBasedAttenuatedBathtub(0.1))
+
+attenuation_rates = zeros(size(fp.elevation) ...) .+ 0.3
+
+for x in 1:size(fp.elevation, 1)
+    for y in 1:size(fp.elevation, 2)
+        if ! fp.mask.sea[x,y]
+            attenuation_rates[x,y] = sqrt(x + y) * 0.05
+        end
+    end
+end
+
+attenuation_rates = GeoArrays.GeoArray(attenuation_rates, fp.elevation.f, fp.elevation.crs)
+
+pba_local = inundate(fp, 2.0, PathBasedAttenuatedBathtub(attenuation_rates))
+
+@info "Inundation model tests completed successfully"
+
+p1 = heatmap(hcb; title = "HCB ", c = cgrad(:blues), colorbar_title = "m")
+p2 = heatmap(pba; title = "PBAB (global attenuation)", c = cgrad(:blues), colorbar_title = "m")
+p3 = heatmap(pba_local; title = "PBAB (local attenuation)", c = cgrad(:blues), colorbar_title = "m")
+
+plot(p1, p2, p3; layout = (1, 3), size = (1500, 400)) |> display
+readline()
 
 
-fp = SpatialFloodProfile(fabdem; seed = findall(wbm .== true)[1], exposures = [pop])
-fpMask = SpatialFloodProfileMask(fp)
-
-inundation = inundate(fp, fpMask, 3.0f0, PathBasedAttenuatedBathtub(0.6f0))
-
-# inundation |> i_ -> GeoArrays.GeoArray(i_, fabdem.f, fabdem.crs) |> plot(c = :blues)
-
-exposure(fp, inundation) 
