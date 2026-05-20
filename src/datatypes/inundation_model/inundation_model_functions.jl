d8d65b88fdf12b5d7848f95f0948f84d40317a5d
lars.enno <lars.enhonsel@googlemail.com>
2026-05-20 11:31:24 +0200

    Added inundate with precomputed distances and slopes

--- src/datatypes/inundation_model/inundation_model_functions.jl
+++ src/datatypes/inundation_model/inundation_model_functions.jl
@@ -39,6 +39,79 @@ function inundate(hspf::HypsometricProfile{DT}, wl::Number, im::IM)::Tuple{Array
     end
 end
 
+function inundate(hspf::DIVACoast.HypsometricProfile{DT}, wl::Real, im::IM, distances::Vector{DT}, slopes::Vector{DT})::Tuple{Vector{DT},Vector{DT}} where {DT<:Real,IM<:DIVACoast.LinearDistanceAttenuatedInundation}
+
+    # Guard clause: If the water level is below the first elevation, nothing is inundated, return the first elevation and the water level as the only point in the profile.
+    if wl <= hspf.elevation[1]
+        return ([hspf.elevation[1]], [DT(wl)])
+    end
+
+    # Get the index of the last elevation that is less than or equal to the water level, 
+    # this will be the maximum index in the elevation profile that we need to consider for inundation 
+    # As this is a sorted array, we can use searchsortedlast for efficient lookup (binary-search = cheap = O(log(n)))
+
+    elev_idx_max = searchsortedlast(hspf.elevation, wl)
+
+    # As we now know the maximum index of the elevation profile that we need to consider, 
+    # we can pre-allocate the return arrays with the appropriate size (elev_idx_max + 1 to account for the potential intersection point)
+    ret_elev = Vector{DT}(undef, elev_idx_max + 1)
+    ret_wl   = Vector{DT}(undef, elev_idx_max + 1)
+
+    # We also now that the first point in the return profile will always be the first elevation and the water level, so we can set that directly
+    # Setting values in a pre-allocated array is cheaper than pushing values to a dynamically growing array, 
+    # as it avoids the overhead of resizing and copying the array when it grows beyond its current capacity
+    ret_elev[1] = hspf.elevation[1]
+    ret_wl[1]   = wl
+
+    # We still need to track the current index, as we could break out of the loop before reaching elev_idx_max,
+    # if the water level is attenuated below the elevation at some point in the profile
+    ind = 1 # Current step in the elevation profile
+
+    # We start at the second point in the elevation profile
+    for i in 2:elev_idx_max
+        
+        # We calculate the cumulative attenuation of the water level at the current point in the elevation profile
+        Δ_wl_att_sum = distances[i] * im.attenuation_rate
+        # We then calculate the attenuated water level at the current point in the elevation profile
+        wl_att = wl - Δ_wl_att_sum
+
+        # No we check if the attenuated water level is still above the elevation at the current point in the elevation profile,
+        # if it is, we add the current point in the elevation profile and the attenuated water level to the return vectors
+        if wl_att >= hspf.elevation[i]
+            ret_elev[i] = hspf.elevation[i]
+            ret_wl[i]   = wl_att
+            ind = i
+        # Else, we hit the point in the elevation profile where the water level is attenuated below the elevation, and we can break out of the loop, 
+        # as we know that no further points in the elevation profile will be inundated
+        else
+            break
+        end
+    end
+
+    # Now we want to determine the exact point of intersection between the water level and the elevation profile
+
+    # Case 1: The loop reached the end of the profile without breaking, which means no intersection needed.
+    if ind == length(hspf.elevation)
+        resize!(ret_elev, ind)  # Remove the last reserved slot for the potential intersection point, as it is not needed
+        resize!(ret_wl, ind) # Remove the last reserved slot for the potential intersection point, as it is not needed
+        return (ret_elev, ret_wl)
+    # Case 2: The loop broke before reaching the end of the profile, which means we need to calculate the intersection point 
+    # and add it to the return vectors
+    else
+
+        # First we calculate the slope of the elevation profile at the point of intersection, 
+        # which is the current index in the elevation profile where the loop broke, multiplied by 1000 to convert from m/m to m/km
+        sl = slopes[ind + 1] * 1000
+        d_intersect = (ret_wl[ind] - hspf.elevation[ind]) / (sl + im.attenuation_rate)
+        intersect_val = hspf.elevation[ind] + sl * d_intersect
+        ret_elev[ind + 1] = intersect_val
+        ret_wl[ind + 1]   = intersect_val
+        resize!(ret_elev, ind + 1)
+        resize!(ret_wl, ind + 1)
+        return (ret_elev, ret_wl)
+    end
+end
+
 function inundate(hspf::HypsometricProfile{DT}, wl::Real, im::IM)::Tuple{Array{DT},Array{DT}} where {DT<:Real,IM<:LinearDistanceAttenuatedInundation}
     if (wl <= hspf.elevation[1])
         return ([hspf.elevation[1]], [wl])
